6b4b184966f077bf4b98470e80a196553d9c8549
1251912798 <1251912798@qq.com>
2024-01-26 23:58:27 +0800

    feat: 更新

--- src/pages/question/Edit/EditHeader/index.tsx
+++ src/pages/question/Edit/EditHeader/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Button, Space, Typography, Input } from 'antd'
+import { Button, Space, Typography, Input, message } from 'antd'
 import { useDispatch } from 'react-redux'
 import { LeftOutlined, CheckOutlined, EditOutlined, LoadingOutlined } from '@ant-design/icons'
 
@@ -10,7 +10,7 @@ import { updatedPageTitle } from '@/store/pageInfoSlice'
 import styles from './EditHeader.module.scss'
 
 import type { ChangeEvent } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import useLoadComponentList from '@/hooks/useLoadComponentList'
 import { useDebounceEffect, useKeyPress, useRequest } from 'ahooks'
 import { updatedQusetionApi } from '@/api/question'
@@ -50,6 +50,7 @@ const TitleComponent = () => {
   )
 }
 
+// 保存按钮
 const PreserveQuestion = () => {
   const { id } = useParams()
   const pageInfo = useGetPageInfo()
@@ -87,6 +88,33 @@ const PreserveQuestion = () => {
   )
 }
 
+// 发布按钮
+const PublishButton = () => {
+  const navigate = useNavigate()
+  const { id } = useParams()
+  const pageInfo = useGetPageInfo()
+  const { componentList = [] } = useLoadComponentList()
+
+  const { loading, run: pub } = useRequest(
+    async () => {
+      if (!id) return
+      await updatedQusetionApi(id, { ...pageInfo, componentList, isPublished: true })
+    },
+    {
+      manual: true,
+      onSuccess() {
+        message.success('发布成功')
+        navigate(`/question/stat/${id}`)
+      },
+    }
+  )
+  return (
+    <Button type="primary" disabled={loading} icon={loading && <LoadingOutlined />} onClick={pub}>
+      发布
+    </Button>
+  )
+}
+
 // 编辑头部
 const EditHeader = () => {
   return (
@@ -106,7 +134,7 @@ const EditHeader = () => {
         <div className={styles.right}>
           <Space>
             <PreserveQuestion />
-            <Button type="primary">发布</Button>
+            <PublishButton />
           </Space>
         </div>
       </div>
